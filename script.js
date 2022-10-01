7adc6842d7afea84c5be7588a690a811c829169f
Aishat Akinyemi <akinyemiomolola99@gmail.com>
2022-10-01 06:55:20 +0100

    Added the next and previous buttons.

--- script.js
+++ script.js
@@ -8,46 +8,47 @@ window.onload =    function displayCon() {
     {
     document.querySelector(".container1-wrap").style.display = "none";
     document.querySelector(".container2-wrap").style.display = "block"
-    }, 8000
+    }, 10000
 )
 }
 
 
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
 
   var i;
   var slides = document.querySelectorAll(".img-item");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-console.log(slideIndex)
-  setTimeout(showSlides, 10000);
-   // Change image every 2 seconds
 
 }
 
-var textIndex = 0;
-showTexts();
+var textIndex = 1;
+showTexts(textIndex);
 
-function showTexts() {
+function showTexts(n) {
 
   var i;
   var texts = document.querySelectorAll(".text");
   for (i = 0; i < texts.length; i++) {
     texts[i].style.display = "none";  
   }
-  textIndex++;
+  if (n > texts.length) {textIndex = 1}    
+  if (n < 1) {textIndex = texts.length}
   if (textIndex > texts.length) {textIndex = 1}
   texts[textIndex-1].style.display = "block";
-console.log(textIndex)
-  setTimeout(showTexts, 10000);
-   // Change image every 2 seconds
 
 }
 
+
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+  showTexts(textIndex += n);
+}
